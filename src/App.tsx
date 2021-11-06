fed9c2a13cbbdf21db7a353dccd892ff5ee492ec
John Chen <john.bm.chen@gmail.com>
2021-11-06 18:19:28 +1300

    feat: added unity integration. does not load. possibly due to compressed .br files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/build.loader.js",
+  dataUrl: "build/build.data.br",
+  frameworkUrl: "build/build.framework.js.br",
+  codeUrl: "build/build.wasm.br",
+});
 
 function App() {
+  const [progression, setProgression] = useState(0);
+
+  useEffect(function () {
+    unityContext.on("progress", function (progression) {
+      setProgression(progression);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Loading {progression * 100} percent...</p>
+      <Unity unityContext={unityContext} />
     </div>
   );
 }
